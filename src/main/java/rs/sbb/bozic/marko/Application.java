dcd002fea9cc75844df8f54c002ee192aea86798
MarkoBozic90 <bozicmarko35@gmail.com>
2023-09-27 10:21:43 +0200

    init message

--- src/main/java/rs/sbb/bozic/marko/Application.java
+++ src/main/java/rs/sbb/bozic/marko/Application.java
@@ -3,6 +3,11 @@ package rs.sbb.bozic.marko;
 public class Application {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        CopyFile copyFile = new CopyFile(args[0],args[1],args[2]);
+//        CopyFile copyFile = new CopyFile("C:\\Users\\bozic\\Documents\\dirs.txt","C:\\Users\\bozic\\Documents\\imgs.txt","C:\\test123321123321");
+
+        copyFile.searchAndPaste();
+
     }
 }
